ab46134962616cdbb4ff72b39e97f8e2dd7464df
williamd5 <66572326+williamd5@users.noreply.github.com>
2021-08-16 22:27:42 +0300

    Create minifyJSON.js

---
+++ .github/workflows/minifyJSON.js
@@ -0,0 +1,10 @@
+"use strict";
+const fs = require("fs"),
+      path = require("path");
+fs.readFile(path.join(__dirname, "../../countries.json"), function (err, data) {
+  if (err) throw new Error(err);
+  fs.writeFileSync(path.join(__dirname, "../../countries.min.json"), JSON.stringify(JSON.parse(data.toString())), function (err) {
+     if (err) throw new Error(err);
+  });
+});
+console.log("Done!");
